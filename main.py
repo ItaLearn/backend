8f423343763a81fc2870c1a5291c5eb669fdc7a7
Pedro willy <pedrowillyferreiradasilva@gmail.com>
2026-06-05 16:09:16 -0300

    Enviando alterações na main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from fastapi import FastAPI, HTTPException, Depends
-from pydantic import BaseModel, Field
-from sqlalchemy.orm import Session
-from database import engine, get_db
+from fastapi import FastAPI
+from database import engine
 import models
 
 from CreateAula import router as create_aula
@@ -10,6 +8,22 @@ from UpdateAula import router as update_aula
 from DeleteAula import router as delete_aula
 
 from CreateUsuario import router as create_usuario
+
+from createMinicurso import router as create_minicurso
+from buscarMinicurso import router as buscar_minicurso
+
+from avaliarMinicurso import router as avaliar_minicurso
+from listarAvaliacao import router as listar_avaliacao
+from deletarAvaliacao import router as deletar_avaliacao
+
+from favoritarMinicurso import router as favoritar_minicurso
+from listarFavorito import router as listar_favorito
+from removerFavorito import router as remover_favorito
+
+from Filtro import router as filtro
+
+from esqueci_senha import router as esqueci_senha
+from redefinir_senha import router as redefinicao_senha
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -21,3 +35,19 @@ app.include_router(update_aula)
 app.include_router(delete_aula)
 
 app.include_router(create_usuario)
+
+app.include_router(create_minicurso)
+app.include_router(buscar_minicurso)
+
+app.include_router(avaliar_minicurso)
+app.include_router(listar_avaliacao)
+app.include_router(deletar_avaliacao)
+
+app.include_router(favoritar_minicurso)
+app.include_router(listar_favorito)
+app.include_router(remover_favorito)
+
+app.include_router(filtro)
+
+app.include_router(esqueci_senha)
+app.include_router(redefinicao_senha)
